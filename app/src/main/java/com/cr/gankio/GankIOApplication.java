dc8c81c6aa07f555dd62961c095be353de0d4a35
Rui Cai <champions1@163.com>
2017-10-30 15:56:16 +0800

    add Application Class

---
+++ app/src/main/java/com/cr/gankio/GankIOApplication.java
@@ -0,0 +1,16 @@
+package com.cr.gankio;
+
+import com.cr.library.BaseApplication;
+
+/**
+ * Created by RUI CAI on 2017/10/30.
+ */
+
+public class GankIOApplication extends BaseApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //TODO
+    }
+}
