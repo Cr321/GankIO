39ed0c4bde3cbc128f65410cae5e7eb41b78ba1b
Rui Cai <champions1@163.com>
2018-01-24 14:38:11 +0800

    Fix the bug that displays the same content in each tab.

--- app/src/main/java/com/cr/gankio/data/network/GankNewsNetworkDataSource.java
+++ app/src/main/java/com/cr/gankio/data/network/GankNewsNetworkDataSource.java
@@ -8,7 +8,9 @@ import com.cr.gankio.data.GankIOService;
 import com.cr.gankio.data.GankNews;
 import com.cr.gankio.data.GankNewsList;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -27,13 +29,13 @@ public class GankNewsNetworkDataSource {
 
 	private static final Object LOCK = new Object();
 	private static GankNewsNetworkDataSource mInstance;
-	private final MutableLiveData<List<GankNews>> data;
+	private final Map<String, MutableLiveData<List<GankNews>>> datas;
 	private final GankIOService gankIOService;
 
 	private static final String baseurl = "http://gank.io/api/";
 
 	private GankNewsNetworkDataSource() {
-		data = new MutableLiveData<>();
+		datas = new HashMap<>();
 		Retrofit retrofit = new Retrofit.Builder()
 				.baseUrl(baseurl)
 				.addConverterFactory(GsonConverterFactory.create())
@@ -52,10 +54,14 @@ public class GankNewsNetworkDataSource {
 
 	public LiveData<List<GankNews>> getGanksNewsList(String type, int num, int page) {
 		Call<GankNewsList> call = gankIOService.getGankNewsList(type, num, page);
+		if (!datas.containsKey(type)) {
+			MutableLiveData<List<GankNews>> data = new MutableLiveData<>();
+			datas.put(type, data);
+		}
 		call.enqueue(new Callback<GankNewsList>() {
 			@Override
 			public void onResponse(Call<GankNewsList> call, Response<GankNewsList> response) {
-				data.setValue(response.body().getResults());
+				datas.get(type).setValue(response.body().getResults());
 			}
 
 			@Override
@@ -63,7 +69,7 @@ public class GankNewsNetworkDataSource {
 				Log.d(TAG, "onFailure: ");
 			}
 		});
-		return data;
+		return datas.get(type);
 	}
 
 }
